6de1cd2360d9b717308a0167776c67d4f638e08f
Cahid <mr.cebizade@gmail.com>
2024-05-16 15:18:03 +0400

    salam

--- app.py
+++ app.py
@@ -7,5 +7,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(100), nullable=False)
-    publication_date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
+    publication_date = db.Column(db.Date, nullable=False, default=datetime.now())
     genre = db.Column(db.String(50))
     isbn = db.Column(db.String(20), unique=True, nullable=False)
 
